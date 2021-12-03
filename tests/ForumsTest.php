1ba175bdb0d170fc3228735518f8891d571b26c6
Ivo Petkov <ivo@ivopetkov.com>
2021-12-03 13:00:55 +0200

    Add unit tests.

---
+++ tests/ForumsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Forums addon for Bear CMS
+ * https://github.com/bearcms/forums-addon
+ * Copyright (c) Amplilabs Ltd.
+ * Free to use under the MIT license.
+ */
+/**
+ * @runTestsInSeparateProcesses
+ */
+class ForumsTest extends BearCMS\AddonTests\PHPUnitTestCase
+{
+    /**
+     * 
+     */
+    public function testBasics()
+    {
+        $app = $this->getApp();
+
+        $this->assertTrue(true);
+    }
+}
