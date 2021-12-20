fa68d79145a781c1c23c48cb1a73a5c49fe6fe25
Ivo Petkov <ivo@ivopetkov.com>
2021-12-20 11:59:53 +0200

    Element "simple-html" mode update.

--- components/bearcmsForumPostsElement.php
+++ components/bearcmsForumPostsElement.php
@@ -11,6 +11,10 @@ use BearFramework\App;
 $app = App::get();
 $context = $app->contexts->get(__DIR__);
 
+$outputType = (string) $component->getAttribute('output-type');
+$outputType = isset($outputType[0]) ? $outputType : 'full-html';
+$isFullHtmlOutputType = $outputType === 'full-html';
+
 $count = strlen((string)$component->count) > 0 ? (int) $component->count : 5;
 if ($count < 1) {
     $count = 1;
@@ -18,24 +22,33 @@ if ($count < 1) {
 $showRepliesCount = strlen((string)$component->showRepliesCount) > 0 ? (int) $component->showRepliesCount > 0 : true;
 $categoryID = $component->categoryID;
 
-$content = '<div class="bearcms-forum-posts-element">';
-$content .= '<component src="file:' . $context->dir . '/components/bearcmsForumPostsElement/forumPostsList.php" count="' . htmlentities($count) . '" categoryID="' . htmlentities((string)$categoryID) . '" showRepliesCount="' . (int) $showRepliesCount . '"/>';
+$content = '<div' . ($isFullHtmlOutputType ? ' class="bearcms-forum-posts-element"' : '') . '>';
 
-$content .= '<div class="bearcms-forum-posts-new-post-button-container">';
-$newPostData = [
-    'serverData' => \BearCMS\Internal\TempClientData::set(['categoryID' => $categoryID])
-];
-$onClick = 'bearCMS.forumPostsElement.openNewPost(' . json_encode($newPostData) . ');';
-$content .= '<a class="bearcms-forum-posts-new-post-button" href="javascript:void(0);" onclick="' . htmlentities($onClick) . '">' . __('bearcms.forumPosts.New post') . '</a>';
+$content .= '<component src="file:' . $context->dir . '/components/bearcmsForumPostsElement/forumPostsList.php" count="' . htmlentities($count) . '" categoryID="' . htmlentities((string)$categoryID) . '" showRepliesCount="' . (int) $showRepliesCount . '" output-type="' . $outputType . '"/>';
+
+if ($isFullHtmlOutputType) {
+    $content .= '<div class="bearcms-forum-posts-new-post-button-container">';
+    $newPostData = [
+        'serverData' => \BearCMS\Internal\TempClientData::set(['categoryID' => $categoryID])
+    ];
+    $onClick = 'bearCMS.forumPostsElement.openNewPost(' . json_encode($newPostData) . ');';
+    $content .= '<a class="bearcms-forum-posts-new-post-button" href="javascript:void(0);" onclick="' . htmlentities($onClick) . '">' . __('bearcms.forumPosts.New post') . '</a>';
+    $content .= '</div>';
+}
+
 $content .= '</div>';
-$content .= '</div>';
-?><html>
-    <head>
-        <link rel="client-packages-embed" name="-bearcms-forums-element">
-        <style>
-            .bearcms-forum-posts-element{word-wrap:break-word;}
-            .bearcms-forum-posts-post-replies-count{float:right;}
-        </style>
-    </head>
-    <body><?= $content ?></body>
-</html>
+
+echo '<html>';
+echo '<head>';
+if ($isFullHtmlOutputType) {
+    echo '<link rel="client-packages-embed" name="-bearcms-forums-element">';
+    echo '<style>';
+    echo '.bearcms-forum-posts-element{word-wrap:break-word;}';
+    echo '.bearcms-forum-posts-post-replies-count{float:right;}';
+    echo '</style>';
+}
+echo '</head>';
+echo '<body>';
+echo $content;
+echo '</body>';
+echo '</html>';
--- components/bearcmsForumPostsElement/forumPostsList.php
+++ components/bearcmsForumPostsElement/forumPostsList.php
@@ -12,6 +12,10 @@ use BearFramework\App;
 $app = App::get();
 $context = $app->contexts->get(__DIR__);
 
+$outputType = (string) $component->getAttribute('output-type');
+$outputType = isset($outputType[0]) ? $outputType : 'full-html';
+$isFullHtmlOutputType = $outputType === 'full-html';
+
 $count = strlen((string)$component->count) > 0 ? (int) $component->count : 5;
 if ($count < 1) {
     $count = 1;
@@ -21,32 +25,32 @@ $categoryID = $component->categoryID;
 
 $forumPosts = new \BearCMS\Internal\Data\Models\ForumPosts();
 $posts = $forumPosts->getList()
-        ->filterBy('categoryID', $categoryID)
-        ->filter(function($forumPost) use ($app) {
-            if ($forumPost->status === 'approved') {
-                return true;
+    ->filterBy('categoryID', $categoryID)
+    ->filter(function ($forumPost) use ($app) {
+        if ($forumPost->status === 'approved') {
+            return true;
+        }
+        if ($forumPost->status === 'pendingApproval') {
+            if ($app->currentUser->exists()) {
+                return $app->currentUser->provider === $forumPost->author['provider'] && $app->currentUser->id === $forumPost->author['id'];
             }
-            if ($forumPost->status === 'pendingApproval') {
-                if ($app->currentUser->exists()) {
-                    return $app->currentUser->provider === $forumPost->author['provider'] && $app->currentUser->id === $forumPost->author['id'];
-                }
-            }
-            return false;
-        })
-        ->sortBy('createdTime', 'desc');
+        }
+        return false;
+    })
+    ->sortBy('createdTime', 'desc');
 $counter = 0;
 echo '<div>';
 foreach ($posts as $post) {
     $postUrl = $app->urls->get(BearCMS\Internal\ForumsData::$forumPagesPathPrefix . \BearCMS\Internal\Utilities::getSlug($post->id, $post->title) . '/');
     $repliesCount = $post->replies->count();
-    echo '<div class="bearcms-forum-posts-post">';
+    echo '<div' . ($isFullHtmlOutputType ? ' class="bearcms-forum-posts-post"' : '') . '>';
     $statusText = '';
     if ($post->status === 'pendingApproval') {
         $statusText = ' (' . __('bearcms.forumPosts.pending approval') . ')';
     }
-    echo '<a class="bearcms-forum-posts-post-title" href="' . htmlentities($postUrl) . '">' . htmlspecialchars($post->title) . $statusText . '</a>';
+    echo '<a' . ($isFullHtmlOutputType ? ' class="bearcms-forum-posts-post-title"' : '') . ' href="' . htmlentities($postUrl) . '">' . htmlspecialchars($post->title) . $statusText . '</a>';
     if ($showRepliesCount) {
-        echo '<div class="bearcms-forum-posts-post-replies-count">' . ($repliesCount === 1 ? __('bearcms.forumPosts.1 reply') : sprintf(__('bearcms.forumPosts.%s replies'), $repliesCount)) . '</div>';
+        echo '<div' . ($isFullHtmlOutputType ? ' class="bearcms-forum-posts-post-replies-count"' : '') . '>' . ($repliesCount === 1 ? __('bearcms.forumPosts.1 reply') : sprintf(__('bearcms.forumPosts.%s replies'), $repliesCount)) . '</div>';
     }
     echo '</div>';
     $counter++;
@@ -54,7 +58,7 @@ foreach ($posts as $post) {
         break;
     }
 }
-if ($count < $posts->count()) {
+if ($isFullHtmlOutputType && $count < $posts->count()) {
     $component = '<component src="file:' . $context->dir . '/components/bearcmsForumPostsElement/forumPostsList.php" count="' . htmlentities($count + 10) . '" categoryID="' . htmlentities((string)$categoryID) . '" />';
     $loadMoreData = [
         'serverData' => \BearCMS\Internal\TempClientData::set(['componentHTML' => $component])
